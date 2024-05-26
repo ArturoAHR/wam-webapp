72dac4d6b9c6d3a7c3f8babb8bc1233c8dece8ea
Arturo Hernández <arturoaureliohr@gmail.com>
2024-05-26 15:38:22 -0400

    WAM-12: Add Soon Page that only appears in production

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Providers } from "@modules/core";
+import { SoonPage } from "./pages/soon/Soon";
 import { Test } from "./pages/test/Test";
 
 export const App = () => {
+  if (import.meta.env.VITE_ENVIRONMENT === "production") {
+    return <SoonPage />;
+  }
+
   return (
     <Providers>
-      <div>
-        <Test />
-      </div>
+      <Test />
     </Providers>
   );
 };
